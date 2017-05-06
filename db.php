9e29fc33ced8716ee4d485f7700f5a5aeb35d851
sbeam11 <sbeam2@students.towson.edu>
2017-05-06 13:57:47 -0400

    Add files via upload

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+/* Database connection settings */
+$host = 'mysql.hostinger.com';
+$user = 'u341414993_rspur';
+$pass = 'Cosc412';
+$db = 'u341414993_proj1';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
--- register.php
+++ register.php
@@ -1,85 +1,67 @@
 <?php
-//database initialization.
-$db_name = "id1296369_software1";
-$db_host = "localhost";
-$db_user = "id1296369_rspurl1";
-$db_pw = "cosc455";
+/* Registration process, inserts user info into the database 
+   and sends account confirmation email message
+ */
 
-//Create connection
-$conn = new mysqli($db_host, $db_user, $db_pw, $db_name);
+// Set session variables to be used on profile.php page
+$_SESSION['email'] = $_POST['email'];
+$_SESSION['first_name'] = $_POST['firstname'];
+$_SESSION['last_name'] = $_POST['lastname'];
 
-//Check connection
-if ($conn->connect_errno) 
-{
-	echo "Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
-	
-} else 
-{
-	echo "Database connection successful" . "<br>";
-	
-	//ensuring button actually sent data to php. 
-	$name = $conn->escape_string ($_POST['name']);
-	$username = $conn->escape_string ($_POST['username']);
-	$email = $conn->escape_string ($_POST['email']);
-	$phone = $conn->escape_string ($_POST['phone']); 
-	$password = $conn->escape_string($_POST['password']);
-	
-	//encrypt password
-	$cost = 10; 
-	$salt = strtr(base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)), '+', '.');
-	$salt = sprintf("$2a$%02d$", $cost) . $salt;
-	$hash = crypt($password, $salt);
+// Escape all $_POST variables to protect against SQL injections
+$first_name = $mysqli->escape_string($_POST['firstname']);
+$last_name = $mysqli->escape_string($_POST['lastname']);
+$email = $mysqli->escape_string($_POST['email']);
+$password = $mysqli->escape_string(password_hash($_POST['password'], PASSWORD_BCRYPT));
+$hash = $mysqli->escape_string( md5( rand(0,1000) ) );
+      
+// Check if user with that email already exists
+$result = $mysqli->query("SELECT * FROM Users WHERE email='$email'") or die($mysqli->error());
 
-	//create verification code
-	$verify = md5($email);
-	
-	//email initialization
-	$to = $email;
-	$subject = "Verfication Code";
-	$message = "Your E-Mail Verification Code is: " .$verify. "<br>" . " please copy and paste into link below: " . "<br>" . "https://afterschoolhours.000webhostapp.com/verification.html";
-	//link goes here. not done yet.
-	$header = "From: no-reply@afterschoolhours.000webhostapp.com";
-	
-	//mailing function
-	$retval = mail ($to, $subject, $message, $header);
-	
-	//check to see if registering user is unique. 
-	$check = $conn->query ("SELECT Email FROM Users WHERE Email = '$email'") or die ($conn->error());
-	$unique = $conn->query ("SELECT Username FROM Users WHERE Username = '$username'") or die ($conn->error());
-	
-	if ($check->num_rows > 0) //checks to see if email is unique, since it's a primary key.
-	{
-		echo "You already have an account!";
-		header('Location: login.html');
-		exit;
-	} else if ($unique->num_rows > 0) //checks to see if username is unique
-	{
-		echo "Username is taken please user another";
-		header ('Location: signup.html');
-		exit;
-	} else 
-	{
-		
-		$insert = $conn->query ("INSERT INTO Users (FullName, Username, Email, Password, vCode, Mobile, Verification) Values ('$name', '$username', '$email', '$hash', '$verify' ,'$phone', 0)");
-		
-		if ($retval == true)
-		{
-			echo "Check your email for verification code!";
-		} else 
-		{
-			echo "Failure";
-		}//check if mail was sent, mainly if server side issue.
-		
-	} // registration else statement
-	
-} //main else statement
+// We know user email exists if the rows returned are more than 0
+if ( $result->num_rows > 0 ) {
+    
+    $_SESSION['message'] = 'User with this email already exists!';
+    header("location: error.php");
+    
+}
+else { // Email doesn't already exist in a database, proceed...
 
+    // active is 0 by DEFAULT (no need to include it here)
+    $sql = "INSERT INTO Users (first_name, last_name, email, password, hash) " 
+            . "VALUES ('$first_name','$last_name','$email','$password', '$hash')";
 
+    // Add user to the database
+    if ( $mysqli->query($sql) ){
 
+        $_SESSION['active'] = 0; //0 until user activates their account with verify.php
+        $_SESSION['logged_in'] = true; // So we know the user has logged in
+        $_SESSION['message'] =
+                
+                 "Confirmation link has been sent to $email, please verify
+                 your account by clicking on the link in the message!";
 
+        // Send registration confirmation link (verify.php)
+        $to      = $email;
+        $subject = 'Account Verification ( clevertechie.com )';
+        $message_body = '
+        Hello '.$first_name.',
 
-//close connection 
-mysqli_close($conn);
-//Written by Andrew Welsh 
+        Thank you for signing up!
 
-?>
+        Please click this link to activate your account:
+
+        http://localhost/login-system/verify.php?email='.$email.'&hash='.$hash;  
+
+        mail( $to, $subject, $message_body );
+
+        header("location: profile.php"); 
+
+    }
+
+    else {
+        $_SESSION['message'] = 'Registration failed!';
+        header("location: error.php");
+    }
+
+}
