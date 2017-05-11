726ced6cf953688a8fd2bcf25fca7d0ee41fc07f
apatel2326 <apatel23@students.towson.edu>
2017-05-10 23:17:52 -0400

    Added comments to show functionality

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 /* Database connection settings */
-$host = 'mysql.hostinger.com';
-$user = 'u341414993_rspur';
-$pass = 'Cosc412';
-$db = 'u341414993_proj1';
-$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+$host = 'mysql.hostinger.com'; //DB host website
+$user = 'u341414993_rspur'; //DB username
+$pass = 'Cosc412';  //DB password
+$db = 'u341414993_proj1'; //DB name
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error); //Log into db 
